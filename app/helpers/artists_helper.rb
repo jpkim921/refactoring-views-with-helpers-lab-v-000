4e2f3fc28c61d2b4cc3a2d9d6a2ab304a44c1f06
Jayon Kim <jpkim921@gmail.com>
2018-08-11 00:48:56 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -12,5 +12,11 @@ module ArtistsHelper
     else
       link_to song.artist.name, artist_path(song.artist)
     end
+
+    if !song.artist
+      link_to "Add Artist", edit_song_path(song)
+    else
+      link_to song.artist.name, artist_path(song.artist)
+    end
   end
 end
